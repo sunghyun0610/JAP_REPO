57806b4afc913636a7ccb425fd105db8e0b0637c
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-03-24 20:47:53 +0900

    dd

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,23 +8,19 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");//데이터베이스 당 하나씩 묶여서 돌아감.
         //entityMangerFactory 만드는 순간 DB연결은 가능
         EntityManager em = emf.createEntityManager();
             //em을 자바컬렉션처럼 이해하면 좋음, 내 객체를 대신 저장해주는 것.
             EntityTransaction tx = em.getTransaction();//JPA 는 transaction이 매우 중요하다
             tx.begin();
-
             try {
-
                 Member member1 =new Member(150L,"A");
                 Member member2 =new Member(160L,"B");
                 em.persist(member1);
                 em.persist(member2);
                 System.out.println("======================");
                 em.persist(member1);//persist 메소드는 새로운 엔티티 인스턴스를 영속성 컨텍스트에 추가하는 역할
-
 //                Member findMember= em.find(Member.class,101L);
 //                System.out.println("findMemeber.id = " + findMember.getId());
 //                System.out.println("findMemeber.name = " + findMember.getName());
@@ -43,7 +39,6 @@ public class JpaMain {
         }finally {
             em.close();//entity manger 꼭 닫아줄 것!
         }
-
         emf.close();
     }
 }
